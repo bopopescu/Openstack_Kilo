b98b8303f172e24eaa25f44caaf8d19eca20ae1b
spring <yuyezhulan@163.com>
2016-04-18 14:44:35 +0800

    add devstack urls

---
+++ linx_local_git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git --init
+#git init --bare 
+git config --global user.name "spring"
+git config --global user.email "yuyezhulan@163.com"
+#git init --bare --shared
+
+#sh-keygen   # enter a few times to accept defaults
+#ssh-copy-id -i ~/.ssh/id_rsa spring@172.16.0.26
+#vi ~/.git/config
+#[receive]
+#        denyCurrentBranch = ignore
+
